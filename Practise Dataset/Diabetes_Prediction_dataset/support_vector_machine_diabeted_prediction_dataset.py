4b3bbc644309ffa6a18a81e44eb05784fb7ba305
Malhar M Pawar <pawarmalhar505@gmail.com>
2023-12-22 08:49:05 +0530

    Minor changes.

--- Practise Dataset/Diabetes_Prediction_dataset/support_vector_machine_diabeted_prediction_dataset.py
+++ Practise Dataset/Diabetes_Prediction_dataset/support_vector_machine_diabeted_prediction_dataset.py
@@ -3,7 +3,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import warnings
 warnings.filterwarnings("ignore")
+
+#read csv file
 df=pd.read_csv("diabetes_prediction_dataset.csv",index_col=0)
+
 dum_df = pd.get_dummies(df,drop_first=True)
 x=dum_df.drop(["diabetes"],axis=1)
 y=dum_df["diabetes"]
@@ -28,3 +31,4 @@ print("Confusion Metrix=",confusion_matrix(y_test, y_pred))                     
                                                                                
 print("Classifiction Report=",classification_report(y_test, y_pred))
 
+print()
